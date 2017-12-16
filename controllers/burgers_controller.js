8641af3e3a6bfc527b994b9b23361343e2a57381
Sam <sjdefrank91@gmail.com>
2017-12-16 00:09:12 -0600

    add public directory, begin front end design

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,12 +5,7 @@ var router = express.Router();
 
 router.get("/", function(req, res){
     burger.allBurgers(function(data){
-        for(i in data){
-            if(data[i].devoured === 0){
-                data[i].devoured = false;
-            }
-        }
-        
+
         res.render("index", {burgers: data})
     });
 });
--- server.js
+++ server.js
@@ -6,16 +6,19 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
+// Set public directory so that server may send files within it
 app.use(express.static("public"));
 
+// I don't know that this line does
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Override POST method if it contains ?_method
 app.use(methodOverride("_method"));
 
-// Set Handlebars.
+
 var exphbs = require("express-handlebars");
 
+// Set the default layout to main.handlbars
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
